64e7b03f8ad20b29f2356e9bd6956b568ba731c5
Youssef Moataz <yousefcrunshy03@gmail.com>
2024-07-27 19:07:56 +0300

    Initial commit

---
+++ app/src/test/java/com/ymoataz/iti/android/recipe_app_android_iti_project/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.ymoataz.iti.android.recipe_app_android_iti_project
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
