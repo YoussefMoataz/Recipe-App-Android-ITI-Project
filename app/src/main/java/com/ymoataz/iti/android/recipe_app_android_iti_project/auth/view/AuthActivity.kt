7a1bd7c5ed20fef7123655ee6601b5c29cf86244
Youssef Moataz <yousefcrunshy03@gmail.com>
2024-07-27 19:22:17 +0300

    Add RecipeActivity

---
+++ app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/auth/view/AuthActivity.kt
@@ -0,0 +1,21 @@
+package com.ymoataz.iti.android.recipe_app_android_iti_project.auth.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ymoataz.iti.android.recipe_app_android_iti_project.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_auth)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/recipe/view/RecipeActivity.kt
+++ app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/recipe/view/RecipeActivity.kt
@@ -1,16 +1,17 @@
-package com.ymoataz.iti.android.recipe_app_android_iti_project
+package com.ymoataz.iti.android.recipe_app_android_iti_project.recipe.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ymoataz.iti.android.recipe_app_android_iti_project.R
 
-class MainActivity : AppCompatActivity() {
+class RecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recipe)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
