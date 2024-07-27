9bc36011f1bc7d2128a7bf7245349d6324ab38bb
Badr MOhamed <badrmohamedragab2003@gmail.com>
2024-07-27 21:38:07 +0300

    Edit design of searchView and refactor the files

--- app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/recipe/search/view/SearchFragment.kt
+++ app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/recipe/search/view/SearchFragment.kt
@@ -1,4 +1,4 @@
-package com.ymoataz.iti.android.recipe_app_android_iti_project.recipe.view
+package com.ymoataz.iti.android.recipe_app_android_iti_project.recipe.search.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
