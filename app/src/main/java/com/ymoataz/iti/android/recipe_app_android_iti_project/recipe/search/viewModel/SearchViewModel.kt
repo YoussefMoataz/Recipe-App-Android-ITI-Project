5bd91e9c6c0439373cc5c53e27609b10870ea438
Badr MOhamed <badrmohamedragab2003@gmail.com>
2024-07-28 20:21:22 +0300

    Add implememtion for SearchRepository , SearchRepositoryImp, SearchViewModel and SearchViewModelFactory

--- app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/recipe/search/viewModel/SearchViewModel.kt
+++ app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/recipe/search/viewModel/SearchViewModel.kt
@@ -1,4 +1,22 @@
 package com.ymoataz.iti.android.recipe_app_android_iti_project.recipe.search.viewModel
 
-class SearchViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ymoataz.iti.android.recipe_app_android_iti_project.recipe.network.dto.MyResponse
+import com.ymoataz.iti.android.recipe_app_android_iti_project.recipe.search.repo.SearchRepository
+import kotlinx.coroutines.launch
+
+class SearchViewModel (private val searchRepository: SearchRepository): ViewModel(){
+    private val _searchResult= MutableLiveData<MyResponse>()
+    val searchResult : LiveData<MyResponse> = _searchResult
+
+    fun search(mealName : String){
+        viewModelScope.launch {
+            val response = searchRepository.search(mealName)
+            _searchResult.value = response
+        }
+    }
+
 }
--- app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/recipe/search/viewModel/SearchViewModelFactory.kt
+++ app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/recipe/search/viewModel/SearchViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.ymoataz.iti.android.recipe_app_android_iti_project.recipe.search.viewModel
 
-class SearchViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ymoataz.iti.android.recipe_app_android_iti_project.recipe.search.repo.SearchRepository
+
+class SearchViewModelFactory (private val searchRepository: SearchRepository):ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if(modelClass.isAssignableFrom(SearchViewModel::class.java))
+        {
+            SearchViewModel(searchRepository) as T
+        }
+        else
+        {
+            throw IllegalArgumentException("")
+        }
+    }
 }
