3590a23bb13f867d34f5d8b8ca37d4dbbeafa1a5
Youssef Moataz <yousefcrunshy03@gmail.com>
2024-08-02 18:48:08 +0300

    Fix RecipeActivity crash.

--- app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/recipe/view/RecipeActivity.kt
+++ app/src/main/java/com/ymoataz/iti/android/recipe_app_android_iti_project/recipe/view/RecipeActivity.kt
@@ -1,3 +1,5 @@
+package com.ymoataz.iti.android.recipe_app_android_iti_project.recipe.view
+
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -6,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.NavDeepLinkBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -23,7 +24,13 @@ class RecipeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_recipe)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
